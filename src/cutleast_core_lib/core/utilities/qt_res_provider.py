4d853bb6723f72376d25d681104da3310d517e01
Cutleast <cutleast@gmail.com>
2026-07-19 23:31:46 +0200

    qt_res_provider.py: Add read_resource_bytes method

--- src/cutleast_core_lib/core/utilities/qt_res_provider.py
+++ src/cutleast_core_lib/core/utilities/qt_res_provider.py
@@ -5,10 +5,43 @@ Copyright (c) Cutleast
 from typing import Any
 
 import jstyleson as json
-from PySide6.QtCore import QFile, QTextStream
+from PySide6.QtCore import QFile
 
 
-def read_resource(name: str) -> str:
+def read_resource_bytes(path: str) -> bytes:
+    """
+    Reads the content of the specified resource.
+
+    **Requires a compiled resource module to be imported!**
+
+    Args:
+        path (str): Resource path to file to read.
+
+    Raises:
+        FileNotFoundError: When the resource does not exist.
+
+    Returns:
+        bytes: Content of the resource.
+    """
+
+    path = path.replace("\\", "/")
+    file = QFile(path)
+
+    if not file.exists():
+        raise FileNotFoundError(path)
+
+    if not file.open(QFile.OpenModeFlag.ReadOnly):
+        raise FileNotFoundError(file.fileName())
+
+    try:
+        content = bytes(file.readAll().data())
+    finally:
+        file.close()
+
+    return content
+
+
+def read_resource(name: str, encoding: str = "utf-8") -> str:
     """
     Reads the content of the specified resource.
 
@@ -16,6 +49,8 @@ def read_resource(name: str) -> str:
 
     Args:
         name (str): Resource path to file to read.
+        encoding (str, optional):
+            Encoding to use when reading the resource. Defaults to "utf-8".
 
     Raises:
         FileNotFoundError: When the resource does not exist.
@@ -24,20 +59,7 @@ def read_resource(name: str) -> str:
         str: Content of the resource.
     """
 
-    name = name.replace("\\", "/")
-    file: QFile = QFile(name)
-
-    if not file.exists():
-        raise FileNotFoundError(name)
-
-    file.open(QFile.OpenModeFlag.ReadOnly | QFile.OpenModeFlag.Text)
-    stream: QTextStream = QTextStream(file)
-
-    content: str = stream.readAll()
-
-    file.close()
-
-    return content
+    return read_resource_bytes(name).decode(encoding)
 
 
 def load_json_resource(name: str) -> Any:
